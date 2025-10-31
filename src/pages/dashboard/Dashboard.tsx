ff59f8725b8bffa84483d49a963a2a391c9232b7
Axel W Notebook <axelwestman@outlook.com>
2025-10-31 11:32:35 -0300

    agregado de dependencias, estructura de proyecto, configuración enrutado

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Dashboard({}: Props) {
+  return (
+    <div className="text-red-500 text-4xl">Dashboard funca 😎</div>
+  )
+}
+
+export default Dashboard
